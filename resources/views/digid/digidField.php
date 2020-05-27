6044d26a75498f538d6ab4d9ba9f012f49049b70
YvetteNikolov <yvettenikolov@gmail.com>
2020-05-27 10:27:59 +0200

    (feat): add extra html title and subtitle to field display

--- resources/views/digid/digidField.php
+++ resources/views/digid/digidField.php
@@ -1,5 +1,11 @@
 <?php if (isset($vars['error'])) : ?>
-<div class="alert" style="border: 1px solid red; font-size: 0.9rem;">{{ error }}</div>
+    <div class="alert" style="border: 1px solid red; font-size: 0.9rem;">{{ error }}</div>
 <?php endif; ?>
 
-<a href="{{ link }}"><img src="{{ logo }}" alt="DigiD logo"></a>
+<a href="{{ link }}" class="digid-btn" style="display: flex; align-items: center;">
+    <img class="digid-btn__img" src="{{ logo }}" alt="DigiD logo">
+    <div class="digid-btn__text" style="padding-left: 0.5rem">
+        <div class="digid-btn__title">{{ title }}</div>
+        <div class="digid-btn__subtitle">{{ subtitle }}</div>
+    </div>
+</a>
