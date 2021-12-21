294192513c0d104e6273285dc87355b3f969fe33
Daniel <daniel@yard.nl>
2021-12-21 14:37:00 +0100

    (build): test

--- resources/js/lib/logoutButton.js
+++ resources/js/lib/logoutButton.js
@@ -3,14 +3,13 @@ export default class LogoutButton {
 		this.logoutBtn = document.querySelector('#logoutLink');
 		this.logoutBtnCopy = document.querySelector('#logoutLink').cloneNode(true);
 		this.gformHeading = document.querySelector('.gform_heading');
-
 	}
 
 	get init() {
 		this.repositionLogoutButton();
 	}
 
-	repositionLogoutButton = () => {
+	repositionLogoutButton() {
 		this.logoutBtn.remove();
 		this.gformHeading.insertAdjacentElement('afterend', this.logoutBtnCopy);
 	}
