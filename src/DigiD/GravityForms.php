dcd051384dd9ae7c01222ef6fce3bbbfa9a42527
Robert Bossaert <contact@robertbossaert.com>
2024-09-02 11:43:09 +0200

    (feat): remove submit button on a form if digid is the only field in it

--- src/DigiD/GravityForms.php
+++ src/DigiD/GravityForms.php
@@ -25,7 +25,7 @@ class GravityForms
     public function clearFormOnFirstRender(array $form): array
     {
         if (!count(array_filter($form['fields'], function ($item) {
-            return  is_a($item, DigiDField::class);
+            return is_a($item, DigiDField::class);
         }))) {
             return $form;
         }
@@ -47,11 +47,43 @@ class GravityForms
     }
 
     /**
+     * Remove the submit button on a form if DigiD is the only field in it.
+     */
+    public function removeSubmitButton(string $button, array $form): string
+    {
+        $specific_field_label = 'digid';
+        $is_specific_field_present = false;
+
+        // Count the number of visible fields in the form
+        $visible_fields_count = 0;
+
+        foreach ($form['fields'] as $field) {
+            // Count only visible fields
+            if (! $field->isHidden) {
+                $visible_fields_count++;
+            }
+
+            // Check if the current field's label matches the specific field label
+            if ($field->type == $specific_field_label && ! $field->isHidden) {
+                $is_specific_field_present = true;
+            }
+        }
+
+        // If the specific field is present, and it's the only visible field, hide the submit button
+        if ($is_specific_field_present && 1 == $visible_fields_count) {
+            return '';
+        }
+
+        // Otherwise, return the original submit button
+        return $button;
+    }
+
+    /**
      * Prepend custom div element for countdown
      */
     public function addCountDownHTML(string $form_tag, array $form): string
     {
-		$bsn = resolve('session')->getSegment('digid')->get('bsn', '');
+        $bsn = resolve('session')->getSegment('digid')->get('bsn', '');
 
         if (!empty($bsn)) {
             $bsn = encrypt($bsn);
@@ -68,38 +100,38 @@ class GravityForms
         return $logout . $form_tag;
     }
 
-	/**
-	 * When multiple IDPs (Identity Providers) are on the same form, the other one is optional.
-	 */
-	public function optionalIDPs(array $result, $value, array $form, \GF_Field $field): array
-	{
-		// Check if there are other IDPs in the session.
-		$eherkenning_in_session = resolve('session')->getSegment('eherkenning')->get('kvk');
-		$eidas_in_session = resolve('session')->getSegment('eidas')->get('bsn');
+    /**
+     * When multiple IDPs (Identity Providers) are on the same form, the other one is optional.
+     */
+    public function optionalIDPs(array $result, $value, array $form, \GF_Field $field): array
+    {
+        // Check if there are other IDPs in the session.
+        $eherkenning_in_session = resolve('session')->getSegment('eherkenning')->get('kvk');
+        $eidas_in_session = resolve('session')->getSegment('eidas')->get('bsn');
 
-		$field_type_digid = 'digid';
-		$field_type_eherkenning = 'eherkenning';
-		$field_type_eidas = 'eidas';
+        $field_type_digid = 'digid';
+        $field_type_eherkenning = 'eherkenning';
+        $field_type_eidas = 'eidas';
 
-		// Check if the form contains the eHerkenning or eIDAS fields.
-		$contains_field = false;
-		foreach ($form['fields'] as $form_field) {
-			if ($form_field->type == $field_type_eherkenning || $field_type_eidas) {
-				$contains_field = true;
-				break;
-			}
-		}
+        // Check if the form contains the eHerkenning or eIDAS fields.
+        $contains_field = false;
+        foreach ($form['fields'] as $form_field) {
+            if ($form_field->type == $field_type_eherkenning || $field_type_eidas) {
+                $contains_field = true;
+                break;
+            }
+        }
 
-		// If eHerkenning or eIDAS are in the session and the form contains the field, DigiD is optional.
-		if (($eherkenning_in_session || $eidas_in_session) && $contains_field) {
-			if ($field->type == $field_type_digid) {
-				$result['is_valid'] = true;
-				$result['message'] = '';
-			}
-		}
+        // If eHerkenning or eIDAS are in the session and the form contains the field, DigiD is optional.
+        if (($eherkenning_in_session || $eidas_in_session) && $contains_field) {
+            if ($field->type == $field_type_digid) {
+                $result['is_valid'] = true;
+                $result['message'] = '';
+            }
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
     protected function hasToken(): bool
     {
